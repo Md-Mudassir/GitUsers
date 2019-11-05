bca534c4aa5f2574b4ebf156ffeb13a97ba845b6
Md-Mudassir <mdmudassirpro@gmail.com>
2019-11-05 15:59:51 +0530

    fontawsome

--- src/comonents/layout/Navbar.js
+++ src/comonents/layout/Navbar.js
@@ -3,9 +3,11 @@ import React, { Component } from "react";
 export class Navbar extends Component {
   render() {
     return (
-      <div>
-        <h1>Yolo</h1>
-      </div>
+      <nav className="navbar bg-primary">
+        <h1>
+          <i className="fab fa-github"> Github Users Finder</i>
+        </h1>
+      </nav>
     );
   }
 }
