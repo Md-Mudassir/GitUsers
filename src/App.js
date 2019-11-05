ee3710285033fd09576be76917b295e21dbbdd90
Md-Mudassir <mdmudassirpro@gmail.com>
2019-11-05 15:48:34 +0530

    component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import Navbar from "./comonents/layout/Navbar";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <Navbar />
       </div>
     );
   }
