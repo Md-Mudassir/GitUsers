24fc19066db837c9d62b2cbe3e1dc7b0fa1e2e2c
Md-Mudassir <mdmudassirpro@gmail.com>
2019-11-05 17:03:47 +0530

    class to functional

--- src/comonents/users/UserItem.js
+++ src/comonents/users/UserItem.js
@@ -1,21 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 
-class UserItem extends Component {
-  render() {
-    const { login, avatar_url, html } = this.props.user;
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          className="round-img"
-          alt=""
-          style={{ width: "60px" }}
-        />
-        <h2>{login}</h2>
-        <a href={html}>Click</a>
-      </div>
-    );
-  }
-}
+const UserItem = props => {
+  const { login, avatar_url, html } = props.user;
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        className="round-img"
+        alt=""
+        style={{ width: "60px" }}
+      />
+      <h2>{login}</h2>
+      <a href={html}>Click</a>
+    </div>
+  );
+};
 
 export default UserItem;
